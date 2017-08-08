ecece8222e44e1b6cfdeb4e0eee638779e4df6fc
Erik Sundberg <esundberg@learning.com>
2017-08-07 22:02:36 -0700

    Added examples of state managers, behaviors, and events/subscriptions

---
+++ Assets/Scripts/Behaviors/MoveBehavior.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBehavior : MonoBehaviour {
+
+	public enum State{
+		Active,
+		Idle
+	}
+
+	State state = State.Idle;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		switch(state){
+		case State.Active:
+			Move();
+			break;
+		case State.Idle:
+			break;
+		}
+	}
+
+	public void Move()
+	{
+		Debug.Log("Character can Move!");
+	}
+
+	public void ChangeState(State newState){
+		state = newState;
+	}
+}
---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	public enum State
+	{
+		Start,
+		Active,
+		Pause,
+		End
+	}
+
+	public delegate void OnGameStateChangeDelegate ();
+	public static event OnGameStateChangeDelegate onGameStartEvent;
+	public static event OnGameStateChangeDelegate onGameEndEvent;
+	public static event OnGameStateChangeDelegate onGamePauseEvent;
+	public static event OnGameStateChangeDelegate onGameResumeEvent;
+
+	State state = State.Start;
+
+	void Start ()
+	{
+		// Send game start event
+		onGameStartEvent();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+
+	void ChangeState(State newSate)
+	{
+		state = newSate;
+	}
+}
