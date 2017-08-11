fde20c474a8d1bc972d6a7f7eb0ffc14ac7f92bf
Chris Freeland <diathis47@gmail.com>
2017-08-10 21:20:59 -0700

    please god

---
+++ Assets/parentToHandle.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class parentToHandle : MonoBehaviour {
+
+    public GameObject parentHandle;
+    public float maxHeight;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float yVal = transform.position.y;
+        yVal = Mathf.Clamp(parentHandle.transform.position.y, 0, maxHeight);
+        transform.position = new Vector3(parentHandle.transform.position.x,yVal,parentHandle.transform.position.z);
+        transform.rotation = parentHandle.transform.rotation;
+    }
+}
