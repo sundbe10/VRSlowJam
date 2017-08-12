92e2039f76f6777232f9ba82fd965af2288f5089
Erik Sundberg <esundberg@learning.com>
2017-08-12 09:44:40 -0700

    Was just messing arounf with string extensions

---
+++ Assets/Scripts/Behaviors/HumanMoveBehavior.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HumanMoveBehavior : MonoBehaviour {
+
+	public enum State{
+		Active,
+		Idle
+	}
+		
+	public float speed = 6.0F;
+	public float jumpSpeed = 8.0F;
+	public float gravity = 20.0F;
+
+	private Vector3 moveDirection = Vector3.zero;
+	private State state;
+
+	void Update() {
+		CharacterController controller = GetComponent<CharacterController>();
+		if (controller.isGrounded) {
+			moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+			transform.rotation = Quaternion.LookRotation(moveDirection.normalized);
+			moveDirection *= speed;
+			if (Input.GetButton("Jump"))
+				moveDirection.y = jumpSpeed;
+			
+
+		}
+		moveDirection.y -= gravity * Time.deltaTime;
+		controller.Move(moveDirection * Time.deltaTime);
+	}
+
+	public void ChangeState(State newState){
+		state = newState;
+	}
+}
---
+++ Assets/Scripts/Managers/HumanManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HumanManager : MonoBehaviour {
+
+	public enum State
+	{
+		Idle,
+		Active,
+		Dead,
+		Celebrate
+	}
+
+	State state = State.Idle;
+	HumanMoveBehavior moveBehavior;
+
+	void Awake ()
+	{
+		// Add event subscription with callback
+		GameManager.onGameStartEvent += EnablePlayer;
+
+		// Get Behaviors
+		moveBehavior = GetComponent<HumanMoveBehavior>();
+	}
+
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+
+	}
+
+	void EnablePlayer()
+	{
+		Debug.Log("Player enabled!");
+		ChangeState(State.Active);
+	}
+
+	void ChangeState(State newState)
+	{
+		state = newState;
+		switch(state)
+		{
+		case State.Idle:
+			break;
+		case State.Active:
+			moveBehavior.ChangeState(HumanMoveBehavior.State.Active);
+			break;
+		case State.Dead: 
+			moveBehavior.ChangeState(HumanMoveBehavior.State.Idle);
+			break;
+		case State.Celebrate: 
+			break;
+		}
+	}
+
+	void OnDestroy()
+	{
+		// Remove event subscription
+		GameManager.onGameStartEvent -= EnablePlayer;
+	}
+}
