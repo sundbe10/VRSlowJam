0b915a41c635fd74f5c5d50054d9737b7d5b8728
Erik Sundberg <esundberg@learning.com>
2017-08-12 12:20:01 -0700

    Add strings to handle

---
+++ Assets/Scripts/LineDrawer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+public class LineDrawer : MonoBehaviour {
+
+	public GameObject startObject;
+	public GameObject endObject;
+	public Color32 lineColor;
+	public float lineWidth = 0.1f;
+
+	LineRenderer lineRenderer;
+
+	// Use this for initialization
+	void Start () {
+		lineRenderer = GetComponent<LineRenderer>();
+		lineRenderer.startColor = lineColor;
+		lineRenderer.endColor = lineColor;
+		lineRenderer.startWidth = lineWidth;
+		lineRenderer.endWidth = lineWidth;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		lineRenderer.SetPosition(0, startObject.transform.position);
+		lineRenderer.SetPosition(1, endObject.transform.position);
+	}
+}
