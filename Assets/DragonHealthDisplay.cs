806aab4c2d1ad13c58a6267c1a132ebb8c495399
Unknown <cameron.a.carson@gmail.com>
2017-08-22 16:30:27 -0700

    ReMerge of Dragon and ControlStick into Main from NewDragonAndVRControls scene
    Also added a turned off Temp Asset Dragon Health UI

---
+++ Assets/DragonHealthDisplay.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragonHealthDisplay : MonoBehaviour {
+
+	public HealthManager BodyHealthManager;
+	public HealthManager HeadHealthManager;
+
+	Canvas canvas;
+	TextMesh text;
+
+	// Use this for initialization
+	void Start () {
+		canvas = GetComponent<Canvas>();
+		text = GetComponentInChildren<TextMesh> ();
+		GameManager.onGameStartEvent += ShowUI;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		float bodyHealth = BodyHealthManager.currentHealth;
+		float headHealth = HeadHealthManager.currentHealth;
+		text.text = "Dragon Body Health = "+bodyHealth+"\nDragon Head Health = "+headHealth;
+	}
+
+	void ShowUI(){
+		canvas.enabled = true;
+	}
+}
